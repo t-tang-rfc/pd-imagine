7484eed5711a60d82327ce142397203bebef26b4
t-tang-rfc <156632897+t-tang-rfc@users.noreply.github.com>
2025-08-07 16:30:52 +0900

    Formalize cascade building in build.sh script

--- build.sh
+++ build.sh
@@ -4,18 +4,69 @@
 
 set -euo pipefail
 
+# === Parameters setting
+
 DOWNLOADS='./downloads'
 ARTIFACTS='./artifacts'
-# Folder structure preparation
-if [ ! -d "$DOWNLOADS" ]; then
-	mkdir "$DOWNLOADS"
-fi
-if [ ! -d "$ARTIFACTS" ]; then
-	mkdir "$ARTIFACTS"
-fi
+QT_ARTIFACT_TAR="${ARTIFACTS}/Qt-6.8.2-install.tar.xz"
 
-# Build Docker image using Dockerfile.ubuntu
-docker build --network=host -f Dockerfile.ubuntu20-build-qt6 -t pd-imagine:ubuntu20-build-qt6 .
+# === Function Definitions
 
-docker_wksp='/wksp'
-docker run --mount "type=bind,source=$(pwd),target=$docker_wksp" pd-imagine:ubuntu20-build-qt6
+create_ros1_qt6_vnc_image() {
+	# Build Docker image using Dockerfile.ros1-qt6-vnc
+	if ! docker image inspect pd-imagine:ros1-qt6-vnc >/dev/null 2>&1; then
+		echo "=== Building Docker image pd-imagine:ros1-qt6-vnc..."
+		# Dependencies
+		create_ros_noetic_dev_image
+		build_qt6
+		# Build the image
+		docker build --network=host -f Dockerfile.ros1-qt6-vnc -t pd-imagine:ros1-qt6-vnc .
+	else
+		echo "[INFO] Docker image pd-imagine:ros1-qt6-vnc already exists, skipping build."
+	fi
+}
+
+create_ros_noetic_dev_image() {
+	# Build Docker image using Dockerfile.ros-noetic-dev
+	if ! docker image inspect pd-imagine:ros-noetic-dev >/dev/null 2>&1; then
+		echo "=== Building Docker image pd-imagine:ros-noetic-dev..."
+		docker build --network=host -f Dockerfile.ros-noetic-dev -t pd-imagine:ros-noetic-dev .
+	else
+		echo "[INFO] Docker image pd-imagine:ros-noetic-dev already exists, skipping build."
+	fi
+}
+
+build_qt6() {
+	if [ ! -f "$QT_ARTIFACT_TAR" ]; then
+		echo "=== Building Qt6 from source..."
+		# Ensure the build environment is set up
+		create_ubuntu20_build_qt6_image
+
+		# Folder structure preparation
+		if [ ! -d "$DOWNLOADS" ]; then
+			mkdir "$DOWNLOADS"
+		fi
+		if [ ! -d "$ARTIFACTS" ]; then
+			mkdir "$ARTIFACTS"
+		fi
+
+		# Build Qt6 using the Docker image
+		docker_wksp='/wksp'
+		docker run --mount "type=bind,source=$(pwd),target=$docker_wksp" pd-imagine:ubuntu20-build-qt6
+	else
+		echo "[INFO] Qt6 artifact tarball already exists at ${QT_ARTIFACT_TAR}, skipping build."
+	fi
+}
+
+create_ubuntu20_build_qt6_image() {
+	# Build Docker image using Dockerfile.ubuntu20-build-qt6
+	if ! docker image inspect pd-imagine:ubuntu20-build-qt6 >/dev/null 2>&1; then
+		echo "=== Building Docker image pd-imagine:ubuntu20-build-qt6..."
+		docker build --network=host -f Dockerfile.ubuntu20-build-qt6 -t pd-imagine:ubuntu20-build-qt6 .
+	else
+		echo "[INFO] Docker image pd-imagine:ubuntu20-build-qt6 already exists, skipping build."
+	fi
+}
+
+# === Main Execution
+create_ros1_qt6_vnc_image
