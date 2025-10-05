4b464d6323940981dac468e1015d1fe30588c868
madpang <133752527+madpang@users.noreply.github.com>
2025-10-05 17:28:12 +0900

    Refine the build.sh script for simplicity
    Add default parameter for `create_ubuntu_build_qt6_image` to make things just work out of box.
    (*) Give up cumbersome differentiation on Qt 6.8 build on Ubuntu 24.04/20.04 -- default to update-to-state version, using legacy version should rely on user's own experience.

--- build.sh
+++ build.sh
@@ -8,17 +8,18 @@ set -euo pipefail
 # === Function Definitions
 
 create_python_dev_image() {
+	local image_name="pd-imagine/python-dev"
 	# Build Docker image using Dockerfile.python-dev
-	if ! docker image inspect pd-imagine:python-dev >/dev/null 2>&1; then
-		echo "=== Building Docker image pd-imagine:python-dev..."
-		docker build --network=host -f Dockerfile.python-dev -t pd-imagine:python-dev .
+	if ! docker image inspect "$image_name" >/dev/null 2>&1; then
+		echo "=== Building Docker image $image_name..."
+		docker build --network=host -f Dockerfile.python-dev -t "$image_name" .
 	else
-		echo "[INFO] Docker image pd-imagine:python-dev already exists, skipping build."
+		echo "[INFO] Docker image $image_name already exists, skipping build."
 	fi
 }
 
 create_ros1_qt6_vnc_image() {
-	local image_name="pd-imagine/ros1-qt6-vnc:latest"
+	local image_name="pd-imagine/ros1-qt6-vnc"
 	# Build Docker image using Dockerfile.ros1-qt6-vnc
 	if ! docker image inspect "$image_name" >/dev/null 2>&1; then
 		echo "=== Building Docker image $image_name..."
@@ -32,8 +33,20 @@ create_ros1_qt6_vnc_image() {
 	fi
 }
 
+create_qt6_dev_vnc_image() {
+	local image_name="pd-imagine/qt6-dev-vnc"
+	# Build Docker image using Dockerfile.qt6-dev-vnc
+	if ! docker image inspect "$image_name" >/dev/null 2>&1; then
+		echo "=== Building Docker image $image_name..."
+		build_qt6_on_ubuntu "24.04"
+		docker build --network=host -f Dockerfile.qt6-dev-vnc -t "$image_name" .
+	else
+		echo "[INFO] Docker image $image_name already exists, skipping build."
+	fi
+}
+
 create_ros_noetic_dev_image() {
-	local image_name="pd-imagine/ros-noetic-dev:latest"
+	local image_name="pd-imagine/ros-noetic-dev"
 	# Build Docker image using Dockerfile.ros-noetic-dev
 	if ! docker image inspect "$image_name" >/dev/null 2>&1; then
 		echo "=== Building Docker image $image_name..."
@@ -43,24 +56,16 @@ create_ros_noetic_dev_image() {
 	fi
 }
 
-create_qt6_dev_vnc_image() {
-	# Build Docker image using Dockerfile.qt6-dev-vnc
-	if ! docker image inspect pd-imagine:qt6-dev-vnc >/dev/null 2>&1; then
-		echo "=== Building Docker image pd-imagine:qt6-dev-vnc..."
-		build_qt6_on_ubuntu "24.04"
-		docker build --network=host -f Dockerfile.qt6-dev-vnc -t pd-imagine:qt6-dev-vnc .
-	else
-		echo "[INFO] Docker image pd-imagine:qt6-dev-vnc already exists, skipping build."
-	fi
-}
-
 build_qt6_on_ubuntu() {
-	# Accept Ubuntu version as argument (should be either 20.04 or 24.04)
+	# @param[in]: Ubuntu version, should be either 20.04 or 24.04
 	local ubuntu_version=${1:-24.04}
-	local image_tag="pd-imagine:ubuntu-${ubuntu_version}-build-qt6"
-	local docker_wksp='/wksp'
+	# Derived variables (@note: MUST match that in )
+	local image_tag="pd-imagine/ubuntu-build-qt6"
+	# Fixed variables
 	local downloads='./downloads'
 	local artifacts='./artifacts'
+	local docker_wksp='/wksp'
+	# @note: The artifact tarball name should match that in the `build_qt.sh` script
 	local QT_ARTIFACT_TAR="${artifacts}/Qt-6.8.2-install.tar.xz"
 
 	if [ ! -f "$QT_ARTIFACT_TAR" ]; then
@@ -72,11 +77,8 @@ build_qt6_on_ubuntu() {
 		if [ ! -d "$artifacts" ]; then
 			mkdir "$artifacts"
 		fi
-
 		# Ensure the build environment is set up based on version
-
-		create_ubuntu_build_qt6_image "$ubuntu_version" "$image_tag"
-		
+		create_ubuntu_build_qt6_image "$ubuntu_version"
 		# Build Qt6 using the appropriate Docker image
 		docker run --mount "type=bind,source=$(pwd),target=$docker_wksp" "$image_tag"
 	else
@@ -85,10 +87,15 @@ build_qt6_on_ubuntu() {
 }
 
 create_ubuntu_build_qt6_image() {
-	# Accept Ubuntu version as argument (should be either 20.04 or 24.04)
-	local ubuntu_version=${1}
-	# Accept image tag as argument
-	local image_tag=${2}
+	# @param[in]: Ubuntu version, should be either 20.04 or 24.04
+	# @note: This parameter will be passed to the Docker build as a build-arg
+	local ubuntu_version=${1:-24.04}
+	if [[ "$ubuntu_version" != "20.04" && "$ubuntu_version" != "24.04" ]]; then
+		echo "Error: Unsupported Ubuntu version '$ubuntu_version'. Supported versions are 20.04 and 24.04."
+		exit 1
+	fi
+	# Derived variables
+	local image_tag="pd-imagine/ubuntu-build-qt6"
 	
 	# Build Docker image using Dockerfile.ubuntu-build-qt6
 	if ! docker image inspect "$image_tag" >/dev/null 2>&1; then
@@ -108,7 +115,8 @@ usage() {
 	echo "  - create_qt6_dev_vnc_image"
 	echo "  - create_ros1_qt6_vnc_image"
 	echo "  - create_ros_noetic_dev_image"
-	echo "  - build_qt6_on_ubuntu [20.04|24.04] (defaults to Ubuntu 24.04)"
+	echo "  - create_ubuntu_build_qt6_image [24.04|20.04] (defaults to Ubuntu 24.04)"
+	echo "  - build_qt6_on_ubuntu [24.04|20.04] (defaults to Ubuntu 24.04)"
 }
 
 # === Main execution logic
